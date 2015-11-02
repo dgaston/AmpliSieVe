9bc33aa5b0a4590796d00aec7fcb2266ae0a9de5
Dan Gaston <daniel.gaston@gmail.com>
2015-11-02 16:04:55 -0400

    Red merging and overlap calculation

--- bin_reads2.py
+++ bin_reads2.py
@@ -1,17 +1,18 @@
 __author__ = 'dgaston'
 
+import os
 import sys
 import csv
 import argparse
 import itertools
 import timeit
+import subprocess as sub
 
 import readfq
 
 from multiprocessing import Pool
 from fuzzywuzzy import fuzz
 from collections import defaultdict
-from nltk.metrics import edit_distance
 
 
 def simple_reverse_complement(seq):
@@ -25,8 +26,6 @@ def simple_reverse_complement(seq):
 
 def read_manifest(infile):
     primers = dict()
-    dlso_dict = dict()
-    ulso_dict = dict()
 
     with open(infile, 'rb') as infh:
         reader = csv.reader(infh, dialect='excel-tab')
@@ -41,14 +40,11 @@ def read_manifest(infile):
             primer_dict['dlso_length'] = len(primer_dict['dlso'])
 
             primers[row[0]] = primer_dict
-
-            ulso_dict[primer_dict['ulso']] = row[0]
-            dlso_dict[primer_dict['dlso']] = row[0]
-    return primers, ulso_dict, dlso_dict
+    return primers
 
 
 def match_read_primers(data):
-    read1, read2, primers, ulso, dlso = data
+    read1, read2, primers  = data
 
     amplicon_assignment = defaultdict(int)
 
@@ -87,10 +83,40 @@ def match_read_primers(data):
     return amplicon_assignment
 
 
+def write_fastq_pairs(file1, file2, read_result):
+    read1_fastq_file = open(file1, "a")
+    read2_fastq_file = open(file2, "a")
+
+    read1_fastq_file.write("@%s\n%s\n+\n%s\n" % (read_result['read1'][0], read_result['read1'][1],
+                                                 read_result['read1'][2]))
+    read2_fastq_file.write("@%s\n%s\n+\n%s\n" % (read_result['read2'][0], read_result['read2'][1],
+                                                 read_result['read2'][2]))
+
+    read1_fastq_file.close()
+    read2_fastq_file.close()
+
+
+def read_bed(infile):
+    amplicons = dict()
+    with open(infile, 'rb') as infh:
+        reader = csv.reader(infh, dialect='excel-tab')
+        reader.next()
+        for row in reader:
+            amplicon_dict = dict()
+            amplicon_dict['chr'] = row[0]
+            amplicon_dict['start'] = int(row[1])
+            amplicon_dict['end'] = int(row[2])
+            amplicon_dict['size'] = int(row[2]) - int(row[1])
+            amplicons[row[3]] = amplicon_dict
+
+    return amplicons
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--infiles', help="Input fastq files, comma-separated with read 1 first [Required]")
     parser.add_argument('-m', '--manifest', help="Manifest CSV file with ULSO and DLSO primer sequences per amplicon")
+    parser.add_argument('-b', '--bed', help="BED file for target amplicons")
+    parser.add_argument('-l', '--length', help="Read length")
     parser.add_argument('-t', '--threads', help="Number of processor threads to use")
     parser.add_argument('-o', '--output', help='Output root for naming output fastq files per amplicon')
 
@@ -100,7 +126,8 @@ if __name__ == "__main__":
     start_time = timeit.default_timer()
 
     sys.stdout.write("Reading primer manifest file\n")
-    primer_sets, ulso_dict, dlso_dict = read_manifest(args.manifest)
+    primer_sets = read_manifest(args.manifest)
+    amplicon_bed = read_bed(args.bed)
 
     fastq_file1 = open(read_files[0], 'r')
     fastq_file2 = open(read_files[1], 'r')
@@ -110,6 +137,7 @@ if __name__ == "__main__":
     read_comparisons = list()
     amplicon_reads = dict()
     amplicon_fastq_file_pairs = dict()
+    no_read_amplicons = list()
 
     num_processed = 0
     num_perfect = 0
@@ -117,51 +145,108 @@ if __name__ == "__main__":
 
     sys.stdout.write("Sorting reads into appropriate amplicons\n")
     for result in pool.imap(match_read_primers, itertools.izip(readfq.readfq(fastq_file1), readfq.readfq(fastq_file2),
-                                                               itertools.repeat(primer_sets),
-                                                               itertools.repeat(ulso_dict),
-                                                               itertools.repeat(dlso_dict)), 100000):
+                                                               itertools.repeat(primer_sets)), 100000):
         num_processed += 1
         if (num_processed % 10000) == 0:
             sys.stdout.write("Processed %s reads\n" % num_processed)
+
         if result['ratio1_amplicon'] != result['ratio2_amplicon']:
             num_mismatched += 1
-            read1_fastq_file = open("%s-mismatched_R1.fastq" % args.output, "a")
-            read2_fastq_file = open("%s-mismatched_R2.fastq" % args.output, "a")
-
-            read1_fastq_file.write("%s\n%s\n+\n%s\n" % (result['read1'][0], result['read1'][1], result['read1'][2]))
-            read2_fastq_file.write("%s\n%s\n+\n%s\n" % (result['read2'][0], result['read2'][1], result['read2'][2]))
-
-            read1_fastq_file.close()
-            read2_fastq_file.close()
+            read1_filename = "%s-mismatched_R1.fastq" % args.output
+            read2_filename = "%s-mismatched_R2.fastq" % args.output
+            write_fastq_pairs(read1_filename, read2_filename, result)
             continue
+
+        read1_fastq_name = "%s-%s_R1.fastq" % (args.output, result['ratio1_amplicon'])
+        read2_fastq_name = "%s-%s_R2.fastq" % (args.output, result['ratio2_amplicon'])
+        write_fastq_pairs(read1_fastq_name, read2_fastq_name, result)
 
         if result['ratio1'] == 100 and result['ratio2'] == 100:
             num_perfect += 1
 
-        read1_fastq_name = "%s-%s_R1.fastq" % (args.output, result['ratio1_amplicon'])
-        read2_fastq_name = "%s-%s_R2.fastq" % (args.output, result['ratio2_amplicon'])
-
         if result['ratio1_amplicon'] not in amplicon_fastq_file_pairs.keys():
             amplicon_files_dict = dict()
             amplicon_files_dict['read1'] = read1_fastq_name
-            amplicon_files_dict['read2'] = read2_fastq_file
+            amplicon_files_dict['read2'] = read2_fastq_name
             amplicon_fastq_file_pairs[result['ratio1_amplicon']] = amplicon_files_dict
-
-        read1_fastq_file = open(read1_fastq_name, "a")
-        read2_fastq_file = open(read2_fastq_name, "a")
-
-        read1_fastq_file.write("%s\n%s\n+\n%s\n" % (result['read1'][0], result['read1'][1], result['read1'][2]))
-        read2_fastq_file.write("%s\n%s\n+\n%s\n" % (result['read2'][0], result['read2'][1], result['read2'][2]))
-
-        read1_fastq_file.close()
-        read2_fastq_file.close()
 
     fastq_file1.close()
     fastq_file2.close()
 
     elapsed = timeit.default_timer() - start_time
     sys.stdout.write("********************************************\n")
-    sys.stdout.write("Total elapsed time for pipeline: %s sec\n" % elapsed)
+    sys.stdout.write("Elapsed time for binning amplicons: %s sec\n" % elapsed)
     sys.stdout.write("Number of mismatched read pairs: %s\n" % num_mismatched)
-    sys.stdout.write("Number of perfectly matching reads: %s" % num_perfect)
+    sys.stdout.write("Number of perfectly matching reads: %s\n" % num_perfect)
+
+    sys.stdout.write("Merging read pairs for each amplicon using Pear\n")
+    for amplicon in primer_sets:
+        output = "%s-%s" % (args.output, amplicon)
+        logfile = "%s-%s-pear-merge.log" % (args.output, amplicon)
+        sys.stdout.write("Merging read pairs for amplicon: %s\n" % amplicon)
+
+        read1 = "%s-%s_R1.fastq" % (args.output, amplicon)
+        read2 = "%s-%s_R2.fastq" % (args.output, amplicon)
+
+        read1gz = "%s.gz" % read1
+        read2gz = "%s.gz" % read2
+
+        bgzip_command1 = ("bgzip %s" % read1)
+        bgzip_command2 = ("bgzip %s" % read2)
+
+        command = ("pear -f %s -r %s -o %s -j %s" % (read1gz, read2gz, output, args.threads))
+
+        if os.path.isfile(read1):
+            with open(logfile, "wb") as err:
+                if not os.path.isfile(read1gz):
+                    p = sub.Popen(bgzip_command1, stdout=sub.PIPE, stderr=err, shell=True)
+                    output = p.communicate()
+                    code = p.returncode
+                    if code:
+                        sys.stdout.write("An error occurred. Please check %s for details\n" % logfile)
+                        sys.stdout.write("%s\n" % output)
+                        sys.stderr.write("An error occurred. Please check %s for details\n" % logfile)
+
+                if not os.path.isfile(read2gz):
+                    p = sub.Popen(bgzip_command2, stdout=sub.PIPE, stderr=err, shell=True)
+                    output = p.communicate()
+                    code = p.returncode
+                    if code:
+                        sys.stdout.write("An error occurred. Please check %s for details\n" % logfile)
+                        sys.stdout.write("%s\n" % output)
+                        sys.stderr.write("An error occurred. Please check %s for details\n" % logfile)
+
+                if not os.path.isfile(output):
+                    p = sub.Popen(command, stdout=sub.PIPE, stderr=err, shell=True)
+                    output = p.communicate()
+                    code = p.returncode
+                    if code:
+                        sys.stdout.write("An error occurred. Please check %s for details\n" % logfile)
+                        sys.stdout.write("%s\n" % output)
+                        sys.stderr.write("An error occurred. Please check %s for details\n" % logfile)
+        else:
+            no_read_amplicons.append(amplicon)
+
+    sys.stdout.write("Calculating expected and observed overlaps for amplicons\n")
+    for amplicon in primer_sets:
+        merged_file = "%s-%s.assembled.fastq" % (args.output, amplicon)
+        overlap_dist = list()
+        overlap_diff_dist = list()
+        expected_overlap = (amplicon_bed[amplicon]['size'] - (2 * int(args.length)) * -1)
+
+        if os.path.isfile(merged_file):
+            sys.stdout.write("Reading file: %s\n" % merged_file)
+            for name, seq, qual in readfq.readfq(merged_file):
+                obs_overlap = (len(seq) - (2 * int(args.length)) * -1)
+                overlap_dist.append(obs_overlap)
+                overlap_diff_dist.append(obs_overlap - expected_overlap)
+
+            sys.stdout.write("Amplicon %s\tExpected Overlap: %s\tMin Obs Overlap: %s\tMax ObsOverlap:%s\n " %
+                             (amplicon, expected_overlap, min(overlap_dist), max(overlap_dist)))
+        elif amplicon in no_read_amplicons:
+            sys.stdout.write("Amplicon %s\tNo Data\n")
+        else:
+            sys.stderr.write("WARNING: Merged file %s for Amplicon %s does not exist but was not logged in no coverage"
+                             "amplicons\n" % (merged_file, amplicon))
+
     sys.stdout.write("Completed pipeline\n")
